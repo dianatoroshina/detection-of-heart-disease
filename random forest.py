5debc558722d3742aacb59936607ed8075f3e468
dianatoroshina <130248922+dianatoroshina@users.noreply.github.com>
2023-07-07 12:01:13 +0300

    Update random forest.py
    fixing errors when importing libraries

--- random forest.py
+++ random forest.py
@@ -1,4 +1,4 @@
-mport pandas as pd
+import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
 from sklearn.metrics import cohen_kappa_score, make_scorer
